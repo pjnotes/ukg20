39891613e5aa6d5f99c3520fbbf756e63cd9c985
pjnotes <67704711+pjnotes@users.noreply.github.com>
2020-07-07 02:01:03 +0530

    Changed redir loc

--- js/pjp.js
+++ js/pjp.js
@@ -4,9 +4,9 @@ $(function () {
 	$("button[type='submit']").click(function () {
 
 		let smd0 = 'ukgHW';
-		let st = 'https://pjnotes.github.io/ukg20/p/';
+		let st = 'https://pjnotes.github.io/p/pages/';
 		st = st+smd0+md5(smd0)+md5(md5(smd0))+'.html';
-		console.log(st);
+		//console.log(st);
 
 		if ( md5( $("#u").val() ) == 'd0e45878043844ffc41aac437e86b602' || md5($("#u").val()) == '30269022e9d8f51beaabb52e5d0de2b7' || md5($("#u").val()) == '654549fd15738332e933b52baf3cbc65' || md5($("#u").val()) == '33b2fa010f5a4dc0ace68c94fd643259' || md5($("#u").val()) == 'a1135b8e9581f4beb118481d65164464' || md5($("#u").val()) == '232423f990a0e50c0675df5a4106911b' ) {
 			console.log("username correct!!");
